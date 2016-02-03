e210ac6ee739e2fa531fff304c98dbe78125e241
iannerubia <ian@qudosmedia.com>
2016-02-03 13:49:08 +0800

    client updates

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,19 @@
 $(document).ready( function() {
-	var lastScroll = 0;
-
 	jQuery(document).ready(function($) {
-	    $(".navbar").addClass("s");
 
-	    $(window).scroll(function(){
-	        setTimeout(function() { //give them a second to finish scrolling before doing a check
-	            var scroll = $(window).scrollTop();
-	            if ( scroll < lastScroll - 70  ) {
-	                $(".navbar").removeClass("navbar-overlay");
-	            } else if (scroll > lastScroll + 70) {
-	                $(".navbar").addClass("navbar-overlay");
-	            }
-	            lastScroll = scroll;
-	        }, 500);
+	    $(window).scroll( function() {
+
+	       	if ( $(this).scrollTop() > 100 ) {
+
+				$(".navbar").addClass("navbar-overlay");
+
+	       	} else {
+
+	       		$(".navbar").removeClass("navbar-overlay");
+	       		
+	       	}
 	    });
 	});
-
 });
 
 
